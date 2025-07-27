f181579c164a00a2c19518e93290cb14b713a542
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-27 18:25:04 +0100

    commit

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,11 +3,14 @@ const Page = async () => {
 
   const data = await res.json();
 
-  console.log(data)
-
   return (
     <div>
       <h1>Cabins Page</h1>
+      <ul>
+        {data.map(user =>(
+            <li key={user.id}>{user.name}</li>
+           ))}
+        </ul>
     </div>
   )
 }
