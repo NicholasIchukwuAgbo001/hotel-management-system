068136eeddc480191b2974211aa15a54795db5a2
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-30 20:30:38 +0100

    commit

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -7,10 +7,10 @@ const Filter = () => {
   const router = useRouter();
   const pathname = usePathname();
 
-  const activeFilter = searchParams.get('capacity') ?? "all";
+  const activeFilter = searchParams.get("capacity") ?? "all";
 
   const handleFilter = (filter) => {
-    const params = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams.toString());
     params.set("capacity", filter);
     router.replace(`${pathname}?${params.toString()}`, { scroll: false });
   };
@@ -23,28 +23,36 @@ const Filter = () => {
   return (
     <div className="flex flex-wrap gap-2 sm:gap-4 justify-center sm:justify-start border border-primary-800 rounded-lg p-2">
       <button
-        className={`${baseBtn} ${activeFilter === "all" ? activeClass : inactiveClass}`}
+        className={`${baseBtn} ${
+          activeFilter === "all" ? activeClass : inactiveClass
+        }`}
         onClick={() => handleFilter("all")}
       >
         All cabins
       </button>
 
       <button
-        className={`${baseBtn} ${activeFilter === "small" ? activeClass : inactiveClass}`}
+        className={`${baseBtn} ${
+          activeFilter === "small" ? activeClass : inactiveClass
+        }`}
         onClick={() => handleFilter("small")}
       >
         1&mdash;3 guests
       </button>
 
       <button
-        className={`${baseBtn} ${activeFilter === "medium" ? activeClass : inactiveClass}`}
+        className={`${baseBtn} ${
+          activeFilter === "medium" ? activeClass : inactiveClass
+        }`}
         onClick={() => handleFilter("medium")}
       >
         4&mdash;7 guests
       </button>
 
       <button
-        className={`${baseBtn} ${activeFilter === "large" ? activeClass : inactiveClass}`}
+        className={`${baseBtn} ${
+          activeFilter === "large" ? activeClass : inactiveClass
+        }`}
         onClick={() => handleFilter("large")}
       >
         8&mdash;12 guests
