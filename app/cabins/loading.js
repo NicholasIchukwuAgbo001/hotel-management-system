ac6a7ac289c5f84453f924c7cdc336ba86bc0d0b
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-30 10:06:27 +0100

    commit

--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -2,7 +2,7 @@ import Spinner from "@/app/_components/Spinner"
 
 const Loading = () => {
   return (
-    <div className="grid items-center justify-center">
+    <div className="grid items-center justify-center pt-10">
         <Spinner />
         <p className="text-xl text-primary-200">Loading cabin data...</p>
     </div>
