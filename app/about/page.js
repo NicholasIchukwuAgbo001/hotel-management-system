83bf752ad96ca57dfb857c085be44975a4e2b8ee
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-31 06:48:19 +0100

    commit

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import image1 from "@/public/about-1.jpg";
-import image2 from "@/public/about-2.jpg";
 import { getCabins } from "../_lib/data-service";
+import Link from "next/link";
 
 export const revalidate = 86400;
 
@@ -16,7 +16,7 @@ export default async function Page() {
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
       <div className="col-span-3">
         <h1 className="text-4xl mb-10 text-accent-400 font-medium">
-          Welcome to The Wild Oasis
+          Welcome to Niko&apos;s Hotel
         </h1>
 
         <div className="space-y-8">
@@ -55,7 +55,7 @@ export default async function Page() {
           src="/about-2.jpg"
           fill
           className="object-cover"
-          alt="Family that manages The Wild Oasis"
+          alt="Family that manages Niko's Hotel"
         />
       </div>
 
@@ -66,27 +66,27 @@ export default async function Page() {
 
         <div className="space-y-8">
           <p>
-            Since 1962, The Wild Oasis has been a cherished family-run retreat.
+            Since 1962, Niko&apos;s Hotel has been a cherished family-run retreat.
             Started by our grandparents, this haven has been nurtured with love
             and care, passing down through our family as a testament to our
             dedication to creating a warm, welcoming environment.
           </p>
           <p>
-            Over the years, we&apos;ve maintained the essence of The Wild Oasis,
+            Over the years, we&apos;ve maintained the essence of Niko&apos;s Hotel,
             blending the timeless beauty of the mountains with the personal
             touch only a family business can offer. Here, you&apos;re not just a
-            guest; you&apos;re part of our extended family. So join us at The
-            Wild Oasis soon, where tradition meets tranquility, and every visit
+            guest; you&apos;re part of our extended family. So join us at Niko&apos;s
+            Hotel soon, where tradition meets tranquility, and every visit
             is like coming home.
           </p>
 
           <div>
-            <a
+            <Link
               href="/cabins"
               className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
             >
               Explore our luxury cabins
-            </a>
+            </Link>
           </div>
         </div>
       </div>
