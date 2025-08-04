be10e03da74d9c4a09c0ebc9fa8dfeb843c93019
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-04 16:42:25 +0100

    commit

--- app/_components/ReservationCard.js
+++ app/_components/ReservationCard.js
@@ -9,7 +9,7 @@ export const formatDistanceFromNow = (dateStr) =>
     addSuffix: true,
   }).replace("about ", "");
 
-function ReservationCard({ booking }) {
+function ReservationCard({ booking, onDelete }) {
   const {
     id,
     guestId,
@@ -80,7 +80,7 @@ function ReservationCard({ booking }) {
               <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
               <span>Edit</span>
             </Link>
-            <DeleteReservation bookingId={id} />
+            <DeleteReservation bookingId={id} onDelete={onDelete} />
           </>
         )}
       </div>
