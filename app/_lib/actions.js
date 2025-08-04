de3e73bfc46b158a35bc61e4b2bccc6634054516
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-04 01:47:09 +0100

    commit

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -25,6 +26,25 @@ export async function updateGuest(formData) {
 
     revalidatePath('/account/profile')
 
+}
+
+export async function deleteReservation(bookingId){
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+
+  const guestBookingsId = guestBookings.map((booking) => booking.id);
+
+  if(!guestBookingsId.includes(bookingId)) throw new Error("your are not allowed to delete this bookings");
+
+  const { error } = await supabase.from("bookings").delete().eq("id", bookingId);
+
+    if (error) {
+     console.error(error);
+     throw new Error("Booking could not be deleted");
+  }
+  revalidatePath('/account/reservations')
 }
 
 export async function signInAction() {
