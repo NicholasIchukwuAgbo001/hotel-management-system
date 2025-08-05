f47e4d26918cb6db7f72967017fd3f945cbdd700
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-05 09:34:48 +0100

    commit

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -37,7 +37,7 @@ function SideNavigation({ onLinkClick }) {
           <li key={link.name}>
             <Link
               href={link.href}
-              onClick={() => onLinkClick?.()} // call onLinkClick if defined
+              onClick={() => onLinkClick?.()}
               className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
                 pathname === link.href ? "bg-primary-900" : ""
               }`}
